fa71c14a0cceaa8a288b67f069f41972c90ac882
Tim Hayes <timmhayes@users.noreply.github.com>
2023-06-24 22:03:29 -0400

    Update Start.tsx
    Fix typo

--- src/Start.tsx
+++ src/Start.tsx
@@ -4,7 +4,7 @@ import libraryImg from './img/library.svg';
 export default function Start() {
   return(
     <>
-      <h2>Seach Libby Faster!</h2>
+      <h2>Search Libby Faster!</h2>
       <Panel title="Instructions">
         <p>The <a href="https://libbyapp.com/">Libby App</a> makes it easy to borrow books from your local library, but searching across multiple libraries with it can be tedious. So we built Libby Search to help out.</p>
         <p>This site requires no login to use. Just link up to all your libraries and search for the books you want. Then go directly to <a href="https://libbyapp.com/">Libby</a> to borrow like normal.</p>
